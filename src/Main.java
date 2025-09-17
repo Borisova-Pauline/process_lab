c243b604ca9d12ad86989a207cbff291070ef854
Pauline <some@mail>
2025-09-17 10:13:04 +0300

    add fourth case

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Что открыть? \n1 - блокнот \n2 - калькулятор \n3 - пейнт");
+        System.out.println("Что открыть? \n1 - блокнот \n2 - калькулятор \n3 - пейнт\n4 - все три");
         String commandOpen;
         String commandClose;
         switch (sc.nextInt()){
@@ -19,6 +19,9 @@ public class Main {
                 commandOpen = "mspaint.exe";
                 commandClose = "taskkill /f /im mspaint.exe";
                 break;
+            case 4:
+                threeProcess();
+                return;
             default:
                 System.out.println("Нет такого");
                 return;
@@ -33,6 +36,29 @@ public class Main {
                 Runtime.getRuntime().exec(commandClose);
             }
         }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+    public static void threeProcess(){
+        Scanner sc = new Scanner(System.in);
+        ProcessBuilder p1 = new ProcessBuilder("notepad.exe");
+        ProcessBuilder p2 = new ProcessBuilder("calc.exe");
+        ProcessBuilder p3 = new ProcessBuilder("mspaint.exe");
+        try{
+            Process process1 = p1.start();
+            Process process2 = p2.start();
+            Process process3 = p3.start();
+            System.out.println(process1.info()+"\n"+process2.info()+"\n"+process3.info());
+            System.out.println("PID блокнота: \t"+process1.toHandle().pid());
+            System.out.println("PID кулькулятора: \t"+process2.toHandle().pid());
+            System.out.println("PID пейнта: \t"+process3.toHandle().pid());
+            System.out.println("Закрыть программы? 1 - да");
+            if(sc.nextInt()==1){
+                Runtime.getRuntime().exec("taskkill /f /im notepad.exe");
+                Runtime.getRuntime().exec("taskkill /f /im CalculatorApp.exe");
+                Runtime.getRuntime().exec("taskkill /f /im mspaint.exe");
+            }
+        }catch(Exception e){
             System.out.println(e.getMessage());
         }
 
